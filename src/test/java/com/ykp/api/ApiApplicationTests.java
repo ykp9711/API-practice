1e95ecfd64a1358cd18db48d4ec1b618bcae0473
ygp <ykp9711@gmail.com>
2023-05-21 19:37:06 +0900

    프로젝트 세팅

---
+++ src/test/java/com/ykp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
